4e40c98a14162aa7c50f25f32ac5614033c199ed
Animesh <animesh.workplace@gmail.com>
2025-09-18 10:54:37 +0530

    updated file - autoencoders/base_ae.py

--- autoencoders/base_ae.py
+++ autoencoders/base_ae.py
@@ -20,10 +20,10 @@ class AutoEncoder(pl.LightningModule):
             nn.ReLU(),
             nn.Linear(8, 4),
             nn.ReLU(),
-            nn.Linear(4, 2),
+            nn.Linear(4, 1),
         )
         self.decoder = nn.Sequential(
-            nn.Linear(2, 4),
+            nn.Linear(1, 4),
             nn.ReLU(),
             nn.Linear(4, 8),
             nn.ReLU(),
